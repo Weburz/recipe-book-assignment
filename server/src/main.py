6b3fd2a412f60c0a485bc9dd43b2158fbaffccca
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2024-07-31 14:19:16 +0530

    chore: add more code

--- server/src/main.py
+++ server/src/main.py
@@ -1,1 +1,13 @@
-print("Hello World!")
+#!/usr/bin/env python3
+
+"""The entrypoint script the uvicorn webserver will invoke."""
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_message() -> dict[str, str]:
+    """Return a "Hello World!" JSON message."""
+    return {"message": "Hello World!"}
